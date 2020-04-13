29fe01bb6cab0e399f575924cd6842f9f4434029
wert <>
2020-04-13 22:12:13 +0800

    添加zuul

---
+++ wolf-cloud-zuul-gateway/src/main/java/com/hyr/filter/PreLogFilter.java
@@ -0,0 +1,56 @@
+package com.hyr.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class PreLogFilter extends ZuulFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreLogFilter.class);
+
+    /**
+     * 过滤器类型，有pre、routing、post、error四种。
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤器执行顺序，数值越小优先级越高。
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 是否进行过滤，返回true会执行过滤。
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 自定义的过滤器逻辑，当shouldFilter()返回true时会执行。
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String remoteHost = request.getRemoteHost();
+        String method = request.getMethod();
+        String requestURI = request.getRequestURI();
+
+        LOGGER.info("Remote host:{},method:{},uri:{}", remoteHost, method, requestURI);
+        return null;
+    }
+}
