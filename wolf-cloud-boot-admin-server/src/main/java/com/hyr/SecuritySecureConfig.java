efcf775a28c580f8f9def35de5ecba491e0fa3bb
wert <>
2020-04-21 23:05:31 +0800

    添加安全监控认证

--- wolf-cloud-boot-admin-server/src/main/java/com/hyr/SecuritySecureConfig.java
+++ wolf-cloud-boot-admin-server/src/main/java/com/hyr/SecuritySecureConfig.java
@@ -1,50 +1,58 @@
-//package com.hyr;
-//
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-//
-//import de.codecentric.boot.admin.server.config.AdminServerProperties;
-//
-//
-////对SpringSecurity进行配置，以便admin-client可以注册：
-//
-//
-////@Configuration
-//public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final String adminContextPath;
-//
-//    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-//        this.adminContextPath = adminServerProperties.getContextPath();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-//        successHandler.setTargetUrlParameter("redirectTo");
-//        successHandler.setDefaultTargetUrl(adminContextPath + "/");
-//
-//        http.authorizeRequests()
-//                //1.配置所有静态资源和登录页可以公开访问
-//                .antMatchers(adminContextPath + "/assets/**").permitAll()
-//                .antMatchers(adminContextPath + "/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                //2.配置登录和登出路径
-//                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-//                .logout().logoutUrl(adminContextPath + "/logout").and()
-//                //3.开启http basic支持，admin-client注册时需要使用
-//                .httpBasic().and()
-//                .csrf()
-//                //4.开启基于cookie的csrf保护
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                //5.忽略这些路径的csrf保护以便admin-client注册
-//                .ignoringAntMatchers(
-//                        adminContextPath + "/instances",
-//                        adminContextPath + "/actuator/**"
-//                );
-//    }
-//
-//}
+package com.hyr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
+
+/****
+ * 限制只有通过HTTP基本认证和登录用户可以使用Admin管理界面。登录界面和静态资源（javascript, HTML, CSS）是公开的，否则无法登录。它是基于cookie的CSRF保护。你可以看到在CSRF保护中有些路径被忽略了，因为Admin Server缺少适当的支持
+
+作者：Java_Explorer
+链接：https://www.jianshu.com/p/1749f04105fb
+来源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * @author m1832
+ *
+ */
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+        http.authorizeRequests()
+                //1.配置所有静态资源和登录页可以公开访问
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                //2.配置登录和登出路径
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                //3.开启http basic支持，admin-client注册时需要使用
+                .httpBasic().and()
+                .csrf()
+                //4.开启基于cookie的csrf保护
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                //5.忽略这些路径的csrf保护以便admin-client注册
+                .ignoringAntMatchers(
+                        adminContextPath + "/instances",
+                        adminContextPath + "/actuator/**"
+                );
+    }
+
+}
